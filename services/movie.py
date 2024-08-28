1585b4dabadd57298ee847fab06cacb98bf55994
olena4reunited <olenaduringintermission@gmail.com>
2024-08-28 13:35:21 +0300

    fix: removed is not None; made all type annotations follow the same code style; used distinct() in return QuerySet not to overwhelm sql queries; fixed import groups positioning; replace get() in delete_movie_session_by_id with filter()

--- services/movie.py
+++ services/movie.py
@@ -1,45 +1,42 @@
 from django.db.models import QuerySet
-from typing import Optional
 
 from db.models import Movie
 
 
 def get_movies(
-        genres_ids: list | None = None,
-        actors_ids: list | None = None
+        genres_ids: list[int] | None = None,
+        actors_ids: list[int] | None = None
 ) -> QuerySet:
     movies = Movie.objects.all()
 
-    if genres_ids is not None:
-        movies = movies.filter(genres__id__in=genres_ids).distinct()
+    if genres_ids:
+        movies = movies.filter(genres__id__in=genres_ids)
 
-    if actors_ids is not None:
-        movies = movies.filter(actors__id__in=actors_ids).distinct()
+    if actors_ids:
+        movies = movies.filter(actors__id__in=actors_ids)
 
-    return movies
+    return movies.distinct()
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
-    return Movie.objects.get(id=movie_id)
+    return Movie.objects.get(pk=movie_id)
 
 
 def create_movie(
         movie_title: str,
         movie_description: str,
-        genres_ids: Optional[list] = None,
-        actors_ids: Optional[list] = None
+        genres_ids: list[int] | None = None,
+        actors_ids: list[int] | None = None
 ) -> Movie:
     movie = Movie.objects.create(
         title=movie_title,
         description=movie_description,
     )
 
-    if genres_ids is not None:
-        for genre_id in genres_ids:
-            movie.genres.add(genre_id)
+    if genres_ids:
+        movie.genres.set(genres_ids)
 
-    if actors_ids is not None:
-        for actor_id in actors_ids:
-            movie.actors.add(actor_id)
+    if actors_ids:
+        movie.actors.set(actors_ids)
 
     return movie
--- services/movie_session.py
+++ services/movie_session.py
@@ -1,6 +1,6 @@
 from datetime import datetime
+
 from django.db.models import QuerySet
-from typing import Optional
 
 from db.models import MovieSession
 
@@ -18,35 +18,34 @@ def create_movie_session(
 
 
 def get_movies_sessions(
-        session_date: Optional[str] = None
+        session_date: str | None = None
 ) -> QuerySet:
     if session_date:
         return MovieSession.objects.filter(
             show_time__date=datetime.strptime(session_date, "%Y-%m-%d")
         )
-    else:
-        return MovieSession.objects.all()
+    return MovieSession.objects.all()
 
 
 def get_movie_session_by_id(
         movie_session_id: int,
 ) -> MovieSession:
-    return MovieSession.objects.get(id=movie_session_id)
+    return MovieSession.objects.get(pk=movie_session_id)
 
 
 def update_movie_session(
         session_id: int,
-        show_time: Optional[datetime] = None,
-        movie_id: Optional[int] = None,
-        cinema_hall_id: Optional[int] = None
+        show_time: datetime | None = None,
+        movie_id: int | None = None,
+        cinema_hall_id: int | None = None
 ) -> MovieSession:
     session_to_update = MovieSession.objects.get(pk=session_id)
 
-    if show_time is not None:
+    if show_time:
         session_to_update.show_time = show_time
-    if movie_id is not None:
+    if movie_id:
         session_to_update.movie_id = movie_id
-    if cinema_hall_id is not None:
+    if cinema_hall_id:
         session_to_update.cinema_hall_id = cinema_hall_id
 
     session_to_update.save()
@@ -57,4 +56,4 @@ def update_movie_session(
 def delete_movie_session_by_id(
         session_id: int
 ) -> None:
-    MovieSession.objects.get(id=session_id).delete()
+    MovieSession.objects.filter(pk=session_id).delete()
